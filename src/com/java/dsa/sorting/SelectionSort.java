a323ddffd5a442859f3e30c4be5d8b74bdc9b728
Manoj K KHare <kkhare.manoj@gmail.com>
2022-03-20 00:03:22 +0530

    Selection sort implementation.

---
+++ src/com/java/dsa/sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.java.dsa.sorting;
+
+import java.util.Arrays;
+
+class SelectionSort {
+    void doSort(int array[]) {
+        System.out.println("Array before sorting : " + Arrays.toString(array));
+        int pass = array.length;
+
+        for (int i = 0; i < pass - 1; i++) {
+            int minElementAtIdx = i;
+            for (int j = i + 1; j < pass; j++)
+                if (array[j] < array[minElementAtIdx])
+                    minElementAtIdx = j;
+
+            int temp = array[minElementAtIdx];
+            array[minElementAtIdx] = array[i];
+            array[i] = temp;
+        }
+        System.out.println("Array after sorting : " + Arrays.toString(array));
+    }
+
+    public static void main(String args[]) {
+        SelectionSort selectionSort = new SelectionSort();
+        int arr[] = {24,78,56,23,19};
+        selectionSort.doSort(arr);
+    }
+}
